765974525c24662503759a2cf6c217a23163e710
Adam Roberts <anasterism@Adams-iMac-5.local>
2019-10-25 08:07:57 -0400

    First commit.

---
+++ database/migrations/2019_10_25_042149_create_locations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->string('street', 255);
+            $table->string('city', 255);
+            $table->string('state', 2);
+            $table->string('zip', 11);
+            $table->string('phone', 14);
+            $table->string('latitude', 255);
+            $table->string('longitude', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
