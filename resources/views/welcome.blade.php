8e71030e6c2f643389e313f66146510f52f8343a
vagrant <vagrant@homestead>
2019-11-10 22:13:05 +0000

    Background style fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,7 @@
     <meta name="robots" content="noindex" />
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>An Asterism - Ohio Bourbon Watch</title>
     <link rel="stylesheet" href="/css/frontend.css">
     @yield('stylesheets')
